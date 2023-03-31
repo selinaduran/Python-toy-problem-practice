5c20459d9f279f30f01cfe2183092d0baff55a1a
selinaduran <selinad177@gmail.com>
2023-03-31 13:32:54 -0500

    completed flippingBits

---
+++ flippingBits/flippingBits.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    binary = format(n, '032b')
+    reversedBinary = ''
+    for c in binary:
+        reversedBinary += '1' if c == '0' else '0'
+    return int(reversedBinary, 2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
